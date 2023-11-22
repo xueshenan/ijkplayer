c47034ca432c5d0530476c862641576c6571c0d2
xueshen.an <xueshen.an@goertek.com>
2023-11-22 11:18:33 +0800

    change do-detect-env.sh min support ndk version to 23

--- android/contrib/tools/do-detect-env.sh
+++ android/contrib/tools/do-detect-env.sh
@@ -35,53 +35,24 @@ if [ -z "$ANDROID_NDK" ]; then
     exit 1
 fi
 
-
-
 # try to detect NDK version
-export IJK_GCC_VER=4.9
-export IJK_GCC_64_VER=4.9
-export IJK_MAKE_TOOLCHAIN_FLAGS=
 export IJK_MAKE_FLAG=
-export IJK_NDK_REL=$(grep -o '^r[0-9]*.*' $ANDROID_NDK/RELEASE.TXT 2>/dev/null | sed 's/[[:space:]]*//g' | cut -b2-)
+export IJK_NDK_REL=$(grep -o '^Pkg\.Revision.*=[0-9]*.*' $ANDROID_NDK/source.properties 2>/dev/null | sed 's/[[:space:]]*//g' | cut -d "=" -f 2)
+echo "IJK_NDK_REL=$IJK_NDK_REL"
+
 case "$IJK_NDK_REL" in
-    10e*)
-        # we don't use 4.4.3 because it doesn't handle threads correctly.
-        if test -d ${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.8
-        # if gcc 4.8 is present, it's there for all the archs (x86, mips, arm)
+    23*|24*|25*|26*)
+        if test -d ${ANDROID_NDK}/toolchains/llvm/prebuilt
         then
             echo "NDKr$IJK_NDK_REL detected"
-
-            case "$UNAME_S" in
-                Darwin)
-                    export IJK_MAKE_TOOLCHAIN_FLAGS="$IJK_MAKE_TOOLCHAIN_FLAGS --system=darwin-x86_64"
-                ;;
-                CYGWIN_NT-*)
-                    export IJK_MAKE_TOOLCHAIN_FLAGS="$IJK_MAKE_TOOLCHAIN_FLAGS --system=windows-x86_64"
-                ;;
-            esac
         else
-            echo "You need the NDKr10e or later"
+            echo "You need the NDKr23c or later"
             exit 1
         fi
     ;;
     *)
-        IJK_NDK_REL=$(grep -o '^Pkg\.Revision.*=[0-9]*.*' $ANDROID_NDK/source.properties 2>/dev/null | sed 's/[[:space:]]*//g' | cut -d "=" -f 2)
-        echo "IJK_NDK_REL=$IJK_NDK_REL"
-        case "$IJK_NDK_REL" in
-            11*|12*|13*|14*)
-                if test -d ${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9
-                then
-                    echo "NDKr$IJK_NDK_REL detected"
-                else
-                    echo "You need the NDKr10e or later"
-                    exit 1
-                fi
-            ;;
-            *)
-                echo "You need the NDKr10e or later"
-                exit 1
-            ;;
-        esac
+        echo "You need the NDKr23c or later"
+        exit 1
     ;;
 esac
 
@@ -89,6 +60,7 @@ esac
 case "$UNAME_S" in
     Darwin)
         export IJK_MAKE_FLAG=-j`sysctl -n machdep.cpu.thread_count`
+        echo "IJK_NAME_FLAG $IJK_NAME_FLAG"
     ;;
     CYGWIN_NT-*)
         IJK_WIN_TEMP="$(cygpath -am /tmp)"
